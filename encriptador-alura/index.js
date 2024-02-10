9a7d1002dc13b5340e3d4236bab8fa9853b51926
Erika Duarte <erika.duarte.mora@gmail.com>
2024-02-10 17:45:47 -0500

    no se aceptan mayusculas, ni acentos ni caracteres especiales

--- encriptador-alura/index.js
+++ encriptador-alura/index.js
@@ -1,46 +1,124 @@
+
 const msje = document.querySelector(".encriptar-mensaje")
 const contenedorEncriptado= document.querySelector(".resultado-encriptado");
 const textareaResultado = document.querySelector(".desencriptar-mensaje")
 const btnEncriptar = document.querySelector(".btn-encriptar")
 const noMensaje = document.querySelector('.no-mensaje')
 const btnCopiar= document.querySelector('.btn-copiar')
-console.log(noMensaje)
+const btnDesencriptar = document.querySelector(".btn-desencriptar")
+const mensajeInf = document.querySelector('.mensajeInfor')
 
-btnEncriptar.addEventListener('click', encriptado)
+let mensajeVisualizado = '';
+let mensajeCopiado =false;
+//Evento del botón encriptar
+btnEncriptar.addEventListener('click', encriptado);
+msje.addEventListener('input', actualizarResultado);
 
 function encriptado() {
-   
     const mensaje = msje.value;
-    let mEncriptado = '';
 
-    for (let i = 0; i < mensaje.length; i++) {
-        if(msje === ''){
-            noMensaje.style = 'display:flex'
+    if (mensaje === '') {
+        noMensaje.style.display = 'flex';
+        contenedorEncriptado.style.display = 'none';
+    } else {
+        const regex = /[A-ZÁÉÍÓÚÜÑáéíóúüñ0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/
+
+        if (regex.test(mensaje)) {
+            contenedorEncriptado.style.display = 'none';
+            mensajeInf.style.color= 'red';
+            noMensaje.style.display = 'flex';
+            
+        alert('El input no debe contener mayúsculas, números o caracteres especiales.');
+        
         }else{
-            if (mensaje[i] === 'e') {
-                mEncriptado += 'enter';
-            } else if (mensaje[i] === 'i') {
-                mEncriptado += 'imes';
-            } else if(mensaje[i] === 'a'){
-                mEncriptado += 'ai';
-            }else if(mensaje[i] === 'o'){
-                mEncriptado += 'ober';
-            }else if(mensaje[i] === 'u'){
-                mEncriptado += 'ufat';
-            }else {
-                mEncriptado += mensaje[i];
+            let mEncriptado = '';
+            for (let i = 0; i < mensaje.length; i++) {
+                if (mensaje[i] === 'e') {
+                    mEncriptado += 'enter';
+                } else if (mensaje[i] === 'i') {
+                    mEncriptado += 'imes';
+                } else if (mensaje[i] === 'a') {
+                    mEncriptado += 'ai';
+                } else if (mensaje[i] === 'o') {
+                    mEncriptado += 'ober';
+                } else if (mensaje[i] === 'u') {
+                    mEncriptado += 'ufat';
+                } else {
+                    mEncriptado += mensaje[i];
+                }
             }
-        }
-    }
-   
-    textareaResultado.textContent = mEncriptado
-    noMensaje.style.display = 'none'
-    contenedorEncriptado.style.display = 'flex'
-    console.log(mEncriptado);
+            mensajeVisualizado = mEncriptado;
+            textareaResultado.textContent = mensajeVisualizado;
+            noMensaje.style.display = 'none';
+            contenedorEncriptado.style.display = 'flex';
+    
+            }
+        }      
+        
 }
+
+//Evento del botón desencriptar
+
+btnDesencriptar.addEventListener('click', desencriptado)
+
+function desencriptado (){
+    const mensaje = msje.value;
+    let mDesencriptado = '';
+    if (mensaje === '') {
+        noMensaje.style.display = 'flex';
+        contenedorEncriptado.style.display = 'none';
+    } else{
+        const palabrasEncriptadas = mensaje.split(' ')
+            for (let i = 0; i < palabrasEncriptadas.length; i++) {
+                    if(palabrasEncriptadas[i]=== 'enter'){
+                        console.log(palabrasEncriptadas[i])
+                        mDesencriptado += 'e';
+                    }else if(palabrasEncriptadas[i] === 'imes'){
+                        mDesencriptado += 'i';
+                    } else if(palabrasEncriptadas[i] === 'ai'){
+                        mDesencriptado += 'a';
+                    } else if(palabrasEncriptadas[i] === 'ober'){
+                        mDesencriptado += 'o';
+                    } else if(palabrasEncriptadas[i] === 'ufat'){
+                        mDesencriptado += 'u';
+                    } else{
+                    mDesencriptado += palabrasEncriptadas[i];
+
+                    }
+                    if (i < palabrasEncriptadas.length - 1) {
+                        mDesencriptado += ' ';
+                    }
+            }
+            mensajeVisualizado = mDesencriptado;
+            textareaResultado.innerHTML = mensajeVisualizado;
+            noMensaje.style.display = 'none';
+            contenedorEncriptado.style.display = 'flex';
+    }
+}
+
+
+
+//Evento del botón copiar
 btnCopiar.addEventListener('click', copiarMensaje)
-function copiarMensaje(){
-    const mensajeCopiado = textareaResultado.value
-    msje.value = ''
-    msje.value= mensajeCopiado
+
+
+function actualizarResultado() {
+    const mensaje = msje.value;
+    if (mensaje === '') {
+        location.reload();
+    } else {
+        textareaResultado.textContent = mensajeVisualizado;
+        noMensaje.style.display = 'none';
+        contenedorEncriptado.style.display = 'flex';
+    }
+    
+}
+
+
+function copiarMensaje() {
+  mensajeCopiado = true;
+  let mensajeVisualizado = textareaResultado.value;
+  msje.value = mensajeVisualizado;
+  textareaResultado.value = "";
+
 }
